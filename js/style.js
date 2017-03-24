357d9cc166cfba3feb5c983dd4406b536d09ae99
badForBoy <731429927@qq.com>
2017-03-24 21:32:07 +0800

    add

--- js/style.js
+++ js/style.js
@@ -1,5 +1,13 @@
 $(function(){
 	var n;
+    $("#secondPage .wrap").css({right: '-500px'});
+    $("#secondPage .title").css({left: '-500px'});
+    $("#thirdPage .wrap").css({right: '-500px'});
+    $("#thirdPage .title").css({left: '-500px'});
+    $("#fourthPage .wrap").css({left: '-500px'});
+    $("#fourthPage .title").css({right: '-500px'});
+    $("#fivePage .wrap").css({left: '-500px'});
+    $("#fivePage .title").css({right: '-500px'});
     $('#dowebok').fullpage({
         verticalCentered: !1,
         navigation: !0,
@@ -7,7 +15,41 @@ $(function(){
             var loadedSection = $(this);
             //using index
             if(index == 1){
-                $("#firstPage .wrap").addClass('move')
+                $("#firstPage .wrap").addClass('move');
+                $(".mun span").css({background: '#fff'});
+            } /*else{
+                $(".mun span").css({background: '#9b9b9b'});
+            }*/
+            else if (index == 2) {
+                $("#secondPage .wrap").animate({right: 0}, 500)
+                $("#secondPage .title").animate({left: 0}, 500)
+                $(".mun span").css({background: '#9b9b9b'});
+            }
+            else if (index == 3) {
+                $("#thirdPage .wrap").animate({right: 0}, 500)
+                $("#thirdPage .title").animate({left: 0}, 500)
+                $("#secondPage .wrap").animate({left: -400}, 500)
+                $("#secondPage .title").animate({right: -400}, 500)
+                $(".mun span").css({background: '#9b9b9b'});
+            }
+            else if (index == 4) {
+                $("#fourthPage .wrap").animate({left: 0}, 500)
+                $("#fourthPage .title").animate({right: 0}, 500)
+                $("#thirdPage .wrap").animate({right: -400}, 500)
+                $("#thirdPage .title").animate({left: -400}, 500)
+                $(".mun span").css({background: '#9b9b9b'});
+            }
+            else if (index == 5) {
+                $("#fivePage .wrap").animate({left: 0}, 500)
+                $("#fivePage .title").animate({right: 0}, 500)
+                $("#fourthPage .wrap").animate({left: -400}, 500)
+                $("#fourthPage .title").animate({right: -400}, 500)
+                $(".mun span").css({background: '#9b9b9b'});
+            }
+            else if (index == 6) {
+                $("#fivePage .wrap").animate({left: -400}, 500)
+                $("#fivePage .title").animate({right: -400}, 500)
+                $(".mun span").css({background: '#9b9b9b'});
             }
         }
     });
@@ -42,7 +84,9 @@ $(function(){
     	}
     });
 
-    $(".header ul li a").click(function(event) {
+    $(".header ul li").click(function(event) {
+        var $height = $("#firstPage").height();
+        var n = $(this).index();
         $(".header ul li a").animate({bottom:'-10'}, 300,function(){
             $(".header ul li").eq(4).find('.box span').animate({right:'-100%'},300)
             $(".header ul li").eq(0).find('.box span').animate({right:'-100%'},300)
